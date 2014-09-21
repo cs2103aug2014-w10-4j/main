c8486bb4001f48381df06d8985a7781297f2cf09
Quan Yang <QuanYang@live.com>
2014-09-21 19:48:25 +0800

    Update to storage interface
    Added more methods

--- src/chirptask/storage/Storage.java
+++ src/chirptask/storage/Storage.java
@@ -1,9 +1,16 @@
 package chirptask.storage;
 
-
 public interface Storage {
-	boolean addTask();
 
-	Task readAllTask();
+	boolean addTask(Task T);
+
+	Task deleteTask(Task T);
+
+	boolean updateTask(Task T);
+	
+	Task getTask(int taskId);
+	
+	// Task array or other data structure(?)
+	Task[] getAllTasks();
 
 }
