15557449bc79e167f15a1e61548bd6aee6226f5f
Quan Yang <QuanYang@live.com>
2014-09-23 00:14:49 +0800

    Modified Task Class added default methods
    and variables

--- src/chirptask/storage/Task.java
+++ src/chirptask/storage/Task.java
@@ -1,9 +1,30 @@
 package chirptask.storage;
 
+import java.util.ArrayList;
+
 //General Interface for the various tasks: Timed Task, Floating Task and Deadline Task.
 
-public interface Task {
+public abstract class Task {
+	private ArrayList<String> _contexts;
+	private ArrayList<String> _categories;
+	private int _taskId;
+	private String _description;
+
+	public Task() {
+		_contexts = new ArrayList<String>();
+		_categories = new ArrayList<String>();
+	}
+
+	public Task(int taskId, String description) {
+		super();
+		_taskId = taskId;
+		_description = description;
+	}
 	
 	
-	
+
+	public int compareTo(Task b) {
+
+		return 0;
+	}
 }
