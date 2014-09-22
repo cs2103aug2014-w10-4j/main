d47043a8fe06ae31510c8d021134d7b6410e7a59
yyhh91 <yonghe91@gmail.com>
2014-09-23 01:06:16 +0800

    Added Logic Class
    Added skeleton for logic class

---
+++ src/chirptask/logic/Logic.java
@@ -0,0 +1,29 @@
+package chirptask.logic;
+
+public class Logic {
+	private static Action lastAction;
+	private static InputParser parser;
+	
+	//Will take in Action object
+	public void executeAction(){
+		
+	}
+	
+	//Filtering according to the UI tag
+	public void filter(){
+		
+	}
+	
+	/*This will update the taskview
+	Retrieve alltasks, sort to date/time, store into Arraylist of dates of arraylist of tasks*/	
+	public void updateTaskView(){
+		
+	}
+	
+	//Take in type, action
+	public void showStatusToUser(){
+		
+	}
+	
+	
+}
