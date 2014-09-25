026607bf714d159c42ad5c8ebc4bd925da0c1e5e
yyhh91 <yonghe91@gmail.com>
2014-09-25 12:38:12 +0800

    Added in updateTaskView()
    Added updateTaskView

--- src/chirptask/logic/Logic.java
+++ src/chirptask/logic/Logic.java
@@ -2,6 +2,10 @@ package chirptask.logic;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 
 import chirptask.storage.Storage;
 import chirptask.storage.Task;
@@ -24,7 +28,7 @@ public class Logic {
 	}
 	
 	//Filtering according to the UI tag
-	public void filter(UI tag){
+	public void filter(String tag){
 		
 	}
 	
@@ -33,12 +37,31 @@ public class Logic {
 	 * Retrieve alltasks, sort to date/time, 
 	 * store into Arraylist of dates of arraylist of tasks
 	 * */	
-	public void updateTaskView(){
-		Storage s = new localStorage();
+	public TaskView updateTaskView(){
+		Storage s = new Storage();
 		//Should change .getAllTasks() to arraylist?
 		ArrayList<Task> allTasks = s.getAllTasks();
  		Collections.sort(allTasks);
+ 		TreeMap<Date, TasksByDate> map = new TreeMap<Date, TasksByDate>();
+ 		
+		for (Task task : allTasks) {
+			Date currDate = task.getDate();
+			if (map.containsKey(currDate)) {
+					map.get(currDate).addToTaskList(task);
+			}
+			else{
+				TasksByDate dateTask = new TasksByDate();
+				dateTask.setTaskDate(currDate);
+				dateTask.addToTaskList(task);
+				map.put(dateTask.getTaskDate(), dateTask);
+			}
+		}
 		
+		Iterator<Map.Entry<Date, TasksByDate>> it = map.entrySet().iterator();
+		TaskView view = new TaskView();
+		while(it.hasNext()){
+			view.addToTaskView(it.next().getValue());
+		}
 	}
 	
 	//Take in type, action
---
+++ src/chirptask/logic/TasksByDate.java
@@ -0,0 +1,32 @@
+package chirptask.logic;
+
+import chirptask.storage.Task;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class TasksByDate {
+	private Date taskDate;
+	private List<Task> taskList;
+	
+	public TasksByDate(){
+		taskList = new ArrayList<Task>();
+	}
+	
+	public Date getTaskDate(){
+		return taskDate;
+	}
+	
+	public List<Task> getTaskList(){
+		return taskList;
+	}
+	
+	public void setTaskDate(Date taskDate){
+		this.taskDate = taskDate;
+	}
+	
+	public void addToTaskList(Task T){
+		taskList.add(T);
+	}
+}
