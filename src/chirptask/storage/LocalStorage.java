ce358088d0c66bce38d4875b438cc21fe395fac9
Ha Linh <katana424@gmail.com>
2014-09-26 16:45:13 +0800

    Refactor a few methods

--- src/chirptask/storage/LocalStorage.java
+++ src/chirptask/storage/LocalStorage.java
@@ -1,7 +1,7 @@
 package chirptask.storage;
 
 import java.io.File;
-import java.text.ParseException;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
@@ -9,6 +9,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
@@ -20,88 +21,119 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class LocalStorage implements Storage {
+	private static final String DATE_FORMAT = "EEE MMM dd HH:mm:SS z yyyy";
+	private static final String XPATH_EXPRESSION = "//task[@TaskId = '%1$s']";
 	File local;
-	DocumentBuilderFactory docBuilderFact = DocumentBuilderFactory.newInstance();
-	TransformerFactory transFact = TransformerFactory.newInstance();
-	
+	DocumentBuilder docBuilder;
+	Transformer trans;
+	Document localStorage;
+
 	public LocalStorage() {
-		Document localStorage;
-		DocumentBuilder docBuilder;
+		localStorageInit();
+		addRoot();
+		writeToFile();
+	}
+
+	/**
+	 * 
+	 */
+	private void localStorageInit() {
 		try {
 			local = new File("local.xml");
-			docBuilder = docBuilderFact.newDocumentBuilder();
+			docBuilder = DocumentBuilderFactory.newInstance()
+					.newDocumentBuilder();
 			localStorage = docBuilder.newDocument();
-			
-			Element rootElement = localStorage.createElement("Tasks");
-			localStorage.appendChild(rootElement);
-			
-			Transformer trans = transFact.newTransformer();
+			trans = TransformerFactory.newInstance().newTransformer();
 			trans.setOutputProperty(OutputKeys.INDENT, "yes");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 
+	 */
+	private void addRoot() {
+		Element rootElement = localStorage.createElement("Tasks");
+		localStorage.appendChild(rootElement);
+	}
+
+	/**
+	 * @throws TransformerException
+	 */
+	private void writeToFile() {
+		try {
+			localStorage.normalize();
 			DOMSource source = new DOMSource(localStorage);
 			StreamResult file = new StreamResult(local);
 			trans.transform(source, file);
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public boolean storeNewTask(Task task) {
-		DocumentBuilder docBuilder;
-		Document parser;
-		try {
-			docBuilder = docBuilderFact.newDocumentBuilder();
-			parser = docBuilder.parse(local);
-			
-			Element root = parser.getDocumentElement();
-			root.appendChild(getTaskNode(parser, task));
-			
-			Transformer trans = transFact.newTransformer();
-			trans.setOutputProperty(OutputKeys.INDENT, "yes");
-			DOMSource source = new DOMSource(parser);
-			StreamResult file = new StreamResult(local);
-			trans.transform(source, file);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
+		Element root = getRoot();
+
+		if (root == null) {
 			return false;
 		}
+
+		root.appendChild(getTaskNode(localStorage, task));
+		writeToFile();
 		return true;
 	}
 
+	/**
+	 * @return
+	 * @throws SAXException
+	 * @throws IOException
+	 */
+	private Element getRoot() {
+		try {
+			localStorage = docBuilder.parse(local);
+			Element root = localStorage.getDocumentElement();
+			return root;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
 	private static Node getTaskNode(Document doc, Task taskToAdd) {
-		Element task = doc.createElement("task");
-		task.setAttribute("TaskId", String.valueOf(taskToAdd.getTaskId()));
-		task.setIdAttribute("TaskId", true);
-		task.setAttribute("event", "no");
-		
-		task.appendChild(getElement(doc, "description", taskToAdd.getDescription()));
-		
+		Element node = doc.createElement("task");
+		node.setAttribute("TaskId", String.valueOf(taskToAdd.getTaskId()));
+		node.setAttribute("event", "no");
+
+		node.appendChild(getElement(doc, "description",
+				taskToAdd.getDescription()));
+
 		ArrayList<String> contexts = taskToAdd.getContexts();
 		if (contexts != null && !contexts.isEmpty()) {
-			for (String s: contexts) {
-				task.appendChild(getElement(doc, "contexts", s));
+			for (String s : contexts) {
+				node.appendChild(getElement(doc, "contexts", s));
 			}
 		}
-		
+
 		ArrayList<String> categories = taskToAdd.getCategories();
 		if (categories != null && !categories.isEmpty()) {
-			for (String s: categories) {
-				task.appendChild(getElement(doc, "categories", s));
+			for (String s : categories) {
+				node.appendChild(getElement(doc, "categories", s));
 			}
 		}
-		
-		task.appendChild(getElement(doc, "date", taskToAdd.getDate().toString()));
-		
+
+		node.appendChild(getElement(doc, "date", taskToAdd.getDate().toString()));
+
 		if (taskToAdd instanceof TimedTask) {
-			task.setAttribute("event", "yes");
+			node.setAttribute("event", "yes");
 			TimedTask timedTask = (TimedTask) taskToAdd;
-			task.appendChild(getElement(doc, "end", timedTask.getEndTime().toString()));
+			node.appendChild(getElement(doc, "end", timedTask.getEndTime()
+					.toString()));
 		}
-		
-		return task;
+
+		return node;
 	}
 
 	private static Node getElement(Document doc, String tag, String value) {
@@ -111,89 +143,56 @@ public class LocalStorage implements Storage {
 	}
 
 	public Task removeTask(Task task) {
-		DocumentBuilder docBuilder;
-		Document parser;
-		Node taskNode;
-
-		try {
-			docBuilder = docBuilderFact.newDocumentBuilder();
-			parser = docBuilder.parse(local);
-			parser.getDocumentElement().normalize();
-			
-			XPathFactory xPathfactory = XPathFactory.newInstance();
-			XPath xpath = xPathfactory.newXPath();
-			
-			String general = "//task[@TaskId = '%1$s']";
-			String expression = String.format(general, String.valueOf(task.getTaskId()));
-
-			taskNode = (Node) xpath.compile(expression).evaluate(parser, XPathConstants.NODE);
-			if (taskNode == null) {
-				return null;
-			}
-			else {
-				Transformer trans = transFact.newTransformer();
-				trans.setOutputProperty(OutputKeys.INDENT, "yes");
-	
-				taskNode.getParentNode().removeChild(taskNode);
-				parser.normalize();
-				
-				DOMSource source = new DOMSource(parser);
-				StreamResult file = new StreamResult(local);
-				trans.transform(source, file);
-			}	
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-		return task;
+		Node taskNode = getTaskNode(task.getTaskId());
+		Task taskToReturn;
 		
+		if (taskNode == null) {
+			return null;
+		} else {
+			taskToReturn = getTaskFromFile(taskNode);
+			taskNode.getParentNode().removeChild(taskNode);
+			writeToFile();
+		}
+		return taskToReturn;
 	}
 
-	@Override
 	public boolean modifyTask(Task T) {
 		Task toDelete = getTask(T.getTaskId());
 		removeTask(toDelete);
 		storeNewTask(T);
 		return true;
 	}
-	
+
 	/**
-	 * This method takes in a number (taskId) and return the task corresponded 
-	 * with taskId
-	 * @param taskId (assume taskId to be unique
+	 * This method takes in a number (taskId) and return the corresponding task
+	 * 
+	 * @param taskId
+	 *            (assume taskId to be unique)
 	 * @return task
 	 */
 	public Task getTask(int taskId) {
 		Node taskNode = getTaskNode(taskId);
 		if (taskNode == null) {
 			return null;
+		} else {
+			return getTaskFromFile(taskNode);
 		}
-		else {
-			try {
-				return getTaskFromFile(taskNode);
-			} catch (ParseException e) {
-				return null;
-			}
-		}
-			
+
 	}
 
 	private Node getTaskNode(int taskId) {
-		DocumentBuilder docBuilder;
-		Document parser;
 		Node taskNode;
 		try {
-			docBuilder = docBuilderFact.newDocumentBuilder();
-			parser = docBuilder.parse(local);
-			parser.getDocumentElement().normalize();
-			
+			localStorage = docBuilder.parse(local);
+			localStorage.getDocumentElement().normalize();
+
 			XPathFactory xPathfactory = XPathFactory.newInstance();
 			XPath xpath = xPathfactory.newXPath();
-			
-			String general = "//task[@TaskId = '%1$s']";
-			String expression = String.format(general, String.valueOf(taskId));
+			String expression = String.format(XPATH_EXPRESSION,
+					String.valueOf(taskId));
 
-			taskNode = (Node) xpath.compile(expression).evaluate(parser, XPathConstants.NODE);
+			taskNode = (Node) xpath.compile(expression).evaluate(localStorage,
+					XPathConstants.NODE);
 
 			if (taskNode == null) {
 				return null;
@@ -207,14 +206,12 @@ public class LocalStorage implements Storage {
 
 	public ArrayList<Task> getAllTasks() {
 		ArrayList<Task> tasks = new ArrayList<Task>();
-		DocumentBuilder docBuilder;
-		Document parser;
-		
+
 		try {
-			docBuilder = docBuilderFact.newDocumentBuilder();
-			parser = docBuilder.parse(local);
-			parser.getDocumentElement().normalize();
-			NodeList taskNodes = parser.getElementsByTagName("task"); 
+
+			localStorage = docBuilder.parse(local);
+			localStorage.getDocumentElement().normalize();
+			NodeList taskNodes = localStorage.getElementsByTagName("task");
 			for (int i = 0; i < taskNodes.getLength(); i++) {
 				tasks.add(getTaskFromFile(taskNodes.item(i)));
 			}
@@ -225,21 +222,26 @@ public class LocalStorage implements Storage {
 		return tasks;
 	}
 
-	private Task getTaskFromFile(Node node) throws ParseException {
+	private Task getTaskFromFile(Node node) {
 		Task task = new Task();
 		if (node.getNodeType() == Node.ELEMENT_NODE) {
 			Element item = (Element) node;
 
 			task.setTaskId(Integer.parseInt(item.getAttribute("TaskId")));
-			task.setDescription(getValue("description", item));
+			task.setDescription(getValues("description", item).get(0));
 			task.setContexts(getValues("contexts", item));
 			task.setCategories(getValues("categories", item));
-			task.setDate(new SimpleDateFormat("EEE MMM dd HH:mm:SS z yyyy").
-					parse(getValue("date", item)));
-			
-			if (item.getAttribute("event") == "yes") {
-				((TimedTask) task).setEndTime(new SimpleDateFormat("EEE MMM dd HH:mm:SS z yyyy").
-						parse(getValue("date", item)));
+
+			try {
+				task.setDate(new SimpleDateFormat(DATE_FORMAT).parse(getValues(
+						"date", item).get(0)));
+
+				if (item.getAttribute("event") == "yes") {
+					((TimedTask) task).setEndTime(new SimpleDateFormat(
+							DATE_FORMAT).parse(getValues("date", item).get(0)));
+				}
+			} catch (Exception e) {
+				return null;
 			}
 		}
 		return task;
@@ -255,14 +257,8 @@ public class LocalStorage implements Storage {
 		return contents;
 	}
 
-	private static String getValue(String tag, Element item) {
-		NodeList nodes = item.getElementsByTagName(tag);
-			Node node = (Node) nodes.item(0);
-			return node.getTextContent();
-	}
-
 	public void close() {
-		
+
 	}
 
 }
