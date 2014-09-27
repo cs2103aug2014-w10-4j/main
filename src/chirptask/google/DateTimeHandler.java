a76311d1718ff99485b5eec7133b3cb67a5ce711
Ming Yi <MingYi@sackboyair-2.local>
2014-09-27 09:58:33 +0800

    Created a class to handle Date and Time
    DateTimeHandler helps to parse user input to the Date object which can
    be used to create a Google DateTime object of the host’s time zone.

---
+++ src/chirptask/google/DateTimeHandler.java
@@ -0,0 +1,59 @@
+package chirptask.google;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import com.google.api.client.util.DateTime;
+
+public class DateTimeHandler {
+    static final String DATE_FORMAT = "yyyy-MM-dd";
+    static final String DEFAULT_DATE = "2015-12-31";
+    
+    static final SimpleDateFormat FORMAT_DATE = 
+            new SimpleDateFormat(DATE_FORMAT);
+    
+    static Date getDateFromInput(String _input) {
+        Date _newDate = null;
+        if (_input == null) {
+            _input = DEFAULT_DATE;
+        }
+        try {
+            _newDate = FORMAT_DATE.parse(_input);
+        } catch (ParseException parseError) {
+            _newDate = getDateFromToday();
+        }
+        return _newDate;
+    }
+    
+    static Date getDateFromToday() {
+        Date _newDate = new Date();
+        return _newDate;
+    }
+    
+    static TimeZone getTimeZoneFromHost() {
+        TimeZone _hostTimeZone = TimeZone.getDefault();
+        return _hostTimeZone;
+    }
+    
+    static DateTime newDateTime(Date _date, TimeZone _timeZone) {
+        DateTime _newDateTime = new DateTime(_date, _timeZone);
+        return _newDateTime;
+    }
+    
+    static DateTime getDateTime() {
+        Date _currentDate = getDateFromToday();
+        TimeZone _hostTimeZone = getTimeZoneFromHost();
+        DateTime _newDateTime = newDateTime(_currentDate, _hostTimeZone);
+        return _newDateTime;
+    }
+    
+    static DateTime getDateTime(String _inputDate) {
+        Date _dateFromInput = getDateFromInput(_inputDate);
+        TimeZone _hostTimeZone = getTimeZoneFromHost();
+        DateTime _newDateTime = newDateTime(_dateFromInput, _hostTimeZone);
+        return _newDateTime;
+    }
+    
+}
