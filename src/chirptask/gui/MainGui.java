d562b09de9505dd10aed43cb16ea5498a590aa1e
Quan Yang <QuanYang@live.com>
2014-09-28 03:24:31 +0800

    remove unused imports from MainGui

--- src/chirptask/gui/MainGui.java
+++ src/chirptask/gui/MainGui.java
@@ -15,7 +15,6 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
