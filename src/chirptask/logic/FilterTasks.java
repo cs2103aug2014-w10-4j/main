e2b4b5709823473b727ecc1b937365c1b17b28ab
yyhh91 <yonghe91@gmail.com>
2014-10-01 00:02:47 +0800

    Updated filterTasks
    Added a condition such that if the currentfilter is empty || "" then we
    will filter the entire task.

--- src/chirptask/logic/FilterTasks.java
+++ src/chirptask/logic/FilterTasks.java
@@ -12,33 +12,38 @@ public class FilterTasks {
 	private StorageHandler _storageHandler;
 	private static List<Task> filteredTask;
 	private static String currentFilter;
-	
+
 	static void filter(Task T) {
 		filteredTask = new ArrayList<Task>();
 		currentFilter = T.getDescription();
 		List<Task> allTask = StorageHandler.getAllTasks();
-		
-		for(Task a : allTask){
-			if (a.getDescription().equalsIgnoreCase(currentFilter)){
+
+		for (Task a : allTask) {
+			if (a.getDescription().equalsIgnoreCase(currentFilter)) {
 				filteredTask.add(a);
 			}
 		}
-		
-	}
-	//Add in filter time, date, task, done, undone
-	//add in checkTaskType
-	static void filter() {
-	    filteredTask = new ArrayList<Task>();
-        List<Task> allTask = StorageHandler.getAllTasks();
-        
-        for(Task a : allTask){
-            if (a.getDescription().equalsIgnoreCase(currentFilter)){
-                filteredTask.add(a);
-            }
-        }
+
 	}
 
-	static List<Task> getFilteredList(){
+	// Add in filter time, date, task, done, undone
+	// add in checkTaskType
+	static void filter() {
+		if (currentFilter.isEmpty() || currentFilter == "") {
+			filteredTask = StorageHandler.getAllTasks();
+		} else {
+			filteredTask = new ArrayList<Task>();
+			List<Task> allTask = StorageHandler.getAllTasks();
+
+			for (Task a : allTask) {
+				if (a.getDescription().equalsIgnoreCase(currentFilter)) {
+					filteredTask.add(a);
+				}
+			}
+		}
+	}
+
+	static List<Task> getFilteredList() {
 		return filteredTask;
 	}
 }
