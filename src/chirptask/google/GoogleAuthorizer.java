b79933d5317e74d6baa68495ad03bfe287947aee
newbishme <bluedabedi@hotmail.com>
2014-10-02 20:16:18 +0800

    Refactor and update GoogleAuthorizer
    Refactored the entire GoogleAuthorizer class.
    
    Updates:
    Removed reading OAuth client secrets from file. Client secrets is now
    embedded it in GoogleAuthorizer class directly.

--- src/chirptask/google/GoogleAuthorizer.java
+++ src/chirptask/google/GoogleAuthorizer.java
@@ -1,16 +1,15 @@
 //@author A0111840W
 package chirptask.google;
 
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.java6.auth.oauth2.VerificationCodeReceiver;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
@@ -28,49 +27,88 @@ import com.google.api.services.tasks.TasksScopes;
  * The current scopes to be granted are: 1) Google Calendar 2) Google Tasks
  */
 public class GoogleAuthorizer {
-    private static final int EXIT_ERROR = 1;
 
-    /** Authorizes the installed application to access user's protected data. */
-    static Credential authorize() throws Exception {
+    /** Authorizes ChirpTask to access user's Google services. */
+    static Credential authorize() throws IOException {
+        String credentialUser = getCredentialUser();
+        String oAuthClientId = getOAuthClientId();
+        String oAuthClientSecret = getOAuthClientSecret();
+        List<String> googleScopes = getGoogleScopesList();
+
+        // Set up Google authorization code flow
+        GoogleAuthorizationCodeFlow codeFlow = getAuthorizationCodeFlow(
+                                            credentialUser,
+                                            oAuthClientId,
+                                            oAuthClientSecret,
+                                            googleScopes);
+        // Authorize with Google using OAuth
+        Credential accessToken = authorizeUsingOAuth(codeFlow, credentialUser);
+        
+        return accessToken;
+    }
+    
+    /**
+     * Set up and return a list of scopes that contains scopes for
+     * Google Tasks and Google Calendar.
+     * @return List<String> googleScopes
+     */
+    private static List<String> getGoogleScopesList() {
         List<String> googleScopes = new ArrayList<String>();
-
-        String chirpUser = "ChirpUser";
-
+        googleScopes.add(CalendarScopes.CALENDAR);
+        googleScopes.add(TasksScopes.TASKS);
+        return googleScopes;
+    }
+    
+    private static String getOAuthClientId() {
+        String oAuthClientId = "157073781842-d8dlmu4d07othjlqegcv7d1pdajso5gv.apps.googleusercontent.com";
+        return oAuthClientId;
+    }
+    
+    private static String getOAuthClientSecret() {
+        String oAuthClientSecret = "Pjd5SdWv-RSE5xKK7TOUQYzK";
+        return oAuthClientSecret;
+    }
+    
+    private static String getCredentialUser() {
+        String credentialUser = "ChirpUser";
+        return credentialUser;
+    }
+    
+    private static GoogleAuthorizationCodeFlow getAuthorizationCodeFlow(
+                                                String credentialUser, 
+                                                String oAuthClientId, 
+                                                String oAuthClientSecret,
+                                                List<String> googleScopes)
+                                               throws IOException {
+        
         FileDataStoreFactory dataStoreFactory = GoogleController._dataStoreFactory;
         HttpTransport httpTransport = GoogleController._httpTransport;
         JsonFactory jsonFactory = GoogleController.JSON_FACTORY;
-
-        // set up FileInputStream for GoogleClientSecrets.load(.., ..) method
-        FileInputStream inputStream = new FileInputStream(
-                "resources/client_secrets.json");
-
-        // load client secrets
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
-                jsonFactory, new InputStreamReader(inputStream));
-
-        // exit if client secrets is default / error
-        if (clientSecrets.getDetails().getClientId().startsWith("Enter")
-                || clientSecrets.getDetails().getClientSecret()
-                        .startsWith("Enter ")) {
-            System.out.println("Enter Client ID and Secret from "
-                    + "https://code.google.com/apis/console/?api=calendar "
-                    + "into " + "resources/client_secrets.json");
-            System.exit(EXIT_ERROR);
-        }
-
-        // set up scopes for Google Calendar and Google Tasks
-        googleScopes.add(CalendarScopes.CALENDAR);
-        googleScopes.add(TasksScopes.TASKS);
-
-        // set up authorization code flow
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                httpTransport, jsonFactory, clientSecrets, googleScopes)
-                .setDataStoreFactory(dataStoreFactory).build();
-
-        inputStream.close();
         
-        // authorize
-        return new AuthorizationCodeInstalledApp(flow,
-                new LocalServerReceiver()).authorize(chirpUser);
+        GoogleAuthorizationCodeFlow codeFlow = new GoogleAuthorizationCodeFlow
+                                                .Builder(httpTransport, 
+                                                        jsonFactory, 
+                                                        oAuthClientId, 
+                                                        oAuthClientSecret, 
+                                                        googleScopes)
+                                                .setDataStoreFactory(
+                                                        dataStoreFactory)
+                                                .build();
+        return codeFlow;
+    }
+    
+    private static Credential authorizeUsingOAuth(
+                                        GoogleAuthorizationCodeFlow codeFlow,
+                                        String credentialUser) 
+                                        throws IOException{
+        
+        VerificationCodeReceiver codeReceiver = new LocalServerReceiver();
+        AuthorizationCodeInstalledApp authorizer = new AuthorizationCodeInstalledApp(
+                                                codeFlow,
+                                                codeReceiver);
+        
+        Credential accessToken = authorizer.authorize(credentialUser);
+        
+        return accessToken;
     }
 }
