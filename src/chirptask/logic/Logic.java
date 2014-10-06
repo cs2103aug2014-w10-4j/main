48465f2ca36108dbeeb341c3f2921ddd2fb5dede
yyhh91 <yonghe91@gmail.com>
2014-10-06 16:52:47 +0800

    Updated logic
    Able to showstatus to user. Done some refactoring and formating of
    codes.

--- src/chirptask/logic/Logic.java
+++ src/chirptask/logic/Logic.java
@@ -19,182 +19,228 @@ import chirptask.google.GoogleController;
 import chirptask.gui.*;
 
 enum CommandType {
-    ADD, DISPLAY, DELETE, EDIT, UNDO, DONE, LOGIN, INVALID, EXIT
+	ADD, DISPLAY, DELETE, EDIT, UNDO, DONE, LOGIN, INVALID, EXIT
 }
 
 enum StatusType {
-    ERROR, MESSAGE
+	ERROR, MESSAGE
 }
 
 public class Logic {
-    private static final String MESSAGE_NEW_COMMAND = "command: ";
-    private static final int ERROR_OPENING_STREAM = 57;
+	private static final String MESSAGE_NEW_COMMAND = "command: ";
+	private static final int ERROR_OPENING_STREAM = 57;
 
-    private Action _lastAction;
-    private InputParser _parser;
-    private StorageHandler _storageHandler;
-    private MainGui _gui;
+	private Action _lastAction;
+	private InputParser _parser;
+	private StorageHandler _storageHandler;
+	private MainGui _gui;
 
-    // For working ChirpTask
-    private BufferedReader commandBufferReader = new BufferedReader(
-            new InputStreamReader(System.in));
+	// For working ChirpTask
+	private BufferedReader commandBufferReader = new BufferedReader(
+			new InputStreamReader(System.in));
 
-    public Logic(MainGui gui) {
+	public Logic(MainGui gui) {
 
-        _storageHandler = new StorageHandler();
-        _parser = new InputParser();
-        _gui = gui;
+		_storageHandler = new StorageHandler();
+		_parser = new InputParser();
+		_gui = gui;
 
-        DisplayView.updateTaskView(_gui);
-    }
+		DisplayView.updateTaskView(_gui);
+	}
 
-    public Logic() {
+	public Logic() {
 
-        _storageHandler = new StorageHandler();
-        _parser = new InputParser();
-        // runUntilExitCommand(); //Temporary CLI code before full integration
-        // with GUI
-        // lastAction = new Action();
+		_storageHandler = new StorageHandler();
+		_parser = new InputParser();
+		// runUntilExitCommand(); //Temporary CLI code before full integration
+		// with GUI
+		// lastAction = new Action();
 
-    }
+	}
 
-    /**
-     * Temporary CLI code before full integration with GUI
-     */
-    private void runUntilExitCommand() {
-        while (true) {
-            issueNewCommandStatement();
-            String userInput = waitForUserCommand();
-            retrieveInputFromUI(userInput);
-        }
-    }
+	/**
+	 * Temporary CLI code before full integration with GUI
+	 */
+	private void runUntilExitCommand() {
+		while (true) {
+			issueNewCommandStatement();
+			String userInput = waitForUserCommand();
+			retrieveInputFromUI(userInput);
+		}
+	}
 
-    private void issueNewCommandStatement() {
-        displayToUser(MESSAGE_NEW_COMMAND);
-    }
+	private void issueNewCommandStatement() {
+		displayToUser(MESSAGE_NEW_COMMAND);
+	}
 
-    private String waitForUserCommand() {
-        String userCommand = getUserCommand();
-        return userCommand;
-    }
+	private String waitForUserCommand() {
+		String userCommand = getUserCommand();
+		return userCommand;
+	}
 
-    private void displayToUser(String messageToDisplay) {
-        System.out.print(messageToDisplay);
-    }
+	private void displayToUser(String messageToDisplay) {
+		System.out.print(messageToDisplay);
+	}
 
-    private String getUserCommand() {
-        try {
-            String userInputCommand = commandBufferReader.readLine();
-            return userInputCommand;
-        } catch (IOException ioAccessError) {
-            exitChirpTask(ERROR_OPENING_STREAM);
-        }
-        return "INVALID";
-    }
+	private String getUserCommand() {
+		try {
+			String userInputCommand = commandBufferReader.readLine();
+			return userInputCommand;
+		} catch (IOException ioAccessError) {
+			exitChirpTask(ERROR_OPENING_STREAM);
+		}
+		return "INVALID";
+	}
 
-    /**
-     * End temporary code
-     */
+	/**
+	 * End temporary code
+	 */
 
-    private void exitChirpTask(int typeOfExit) {
-        System.exit(typeOfExit);
-    }
+	private void exitChirpTask(int typeOfExit) {
+		System.exit(typeOfExit);
+	}
 
-    public void retrieveInputFromUI(String input) {
-        _parser.receiveInput(input);
-        processGroupAction(_parser.getActions().getActionList());
-    }
+	public void retrieveInputFromUI(String input) {
+		_parser.receiveInput(input);
+		processGroupAction(_parser.getActions().getActionList());
+	}
 
-    public void processGroupAction(List<Action> list) {
-        for (Action a : list) {
-            executeAction(a);
-        }
-    }
+	public void processGroupAction(List<Action> list) {
+		for (Action a : list) {
+			executeAction(a);
+		}
+	}
 
-    private CommandType determineCommandType(String commandTypeString) {
-        if (commandTypeString.equalsIgnoreCase("add")) {
-            return CommandType.ADD;
-        } else if (commandTypeString.equalsIgnoreCase("display")) {
-            return CommandType.DISPLAY;
-        } else if (commandTypeString.equalsIgnoreCase("delete")) {
-            return CommandType.DELETE;
-        } else if (commandTypeString.equalsIgnoreCase("edit")) {
-            return CommandType.EDIT;
-        } else if (commandTypeString.equalsIgnoreCase("undo")) {
-            return CommandType.UNDO;
-        } else if (commandTypeString.equalsIgnoreCase("done")) {
-            return CommandType.DONE;
-        } else if (commandTypeString.equalsIgnoreCase("login")) {
-            return CommandType.LOGIN;
-        } else if (commandTypeString.equalsIgnoreCase("exit")) {
-            return CommandType.EXIT;
-        } else {
-            return CommandType.INVALID;
-        }
-    }
+	private CommandType determineCommandType(String commandTypeString) {
+		if (commandTypeString.equalsIgnoreCase("add")) {
+			return CommandType.ADD;
+		} else if (commandTypeString.equalsIgnoreCase("display")) {
+			return CommandType.DISPLAY;
+		} else if (commandTypeString.equalsIgnoreCase("delete")) {
+			return CommandType.DELETE;
+		} else if (commandTypeString.equalsIgnoreCase("edit")) {
+			return CommandType.EDIT;
+		} else if (commandTypeString.equalsIgnoreCase("undo")) {
+			return CommandType.UNDO;
+		} else if (commandTypeString.equalsIgnoreCase("done")) {
+			return CommandType.DONE;
+		} else if (commandTypeString.equalsIgnoreCase("login")) {
+			return CommandType.LOGIN;
+		} else if (commandTypeString.equalsIgnoreCase("exit")) {
+			return CommandType.EXIT;
+		} else {
+			return CommandType.INVALID;
+		}
+	}
 
-    // Will take in Action object
-    public void executeAction(Action command) {
-        String action = command.getCommandType();
-        CommandType actionType = determineCommandType(action);
-        Task task = command.getTask();
-        switch (actionType) {
-            case ADD :
-                _storageHandler.addTask(task);
-                this.setLastAction(command);
-                FilterTasks.filter();
-                DisplayView.updateTaskView(FilterTasks.getFilteredList(), _gui);
-                break;
-            case DELETE :
-                _storageHandler.deleteTask(task);
-                this.setLastAction(command);
-                FilterTasks.filter();
-                DisplayView.updateTaskView(FilterTasks.getFilteredList(), _gui);
-                break;
-            case DISPLAY :
-                // now can only filter string
-                FilterTasks.filter(task);
-                DisplayView.updateTaskView(FilterTasks.getFilteredList(), _gui);
-                break;
-            case EDIT :
-                _storageHandler.modifyTask(task);
-                this.setLastAction(command);
-                FilterTasks.filter();
-                DisplayView.updateTaskView(FilterTasks.getFilteredList(), _gui);
-                break;
-            case UNDO :
-                // negate action and run excecuteAction again
-                executeAction(command.undo(this.getLastAction()));
-                FilterTasks.filter();
-                DisplayView.updateTaskView(FilterTasks.getFilteredList(), _gui);
-                break;
-            case DONE :
-                task.setDone(true);
-                _storageHandler.modifyTask(task);
-                this.setLastAction(command);
-                FilterTasks.filter();
-                DisplayView.updateTaskView(FilterTasks.getFilteredList(), _gui);
-                break;
-            case LOGIN :
-                _storageHandler.initCloudStorage();
-                break;
-            case EXIT :
-                System.exit(0);
-                break;
-            case INVALID :
-                // call print some invalid message
-                break;
-            default:
-                // throw error
-        }
-    }
+	// Will take in Action object
+	public void executeAction(Action command) {
+		String action = command.getCommandType();
+		CommandType actionType = determineCommandType(action);
+		Task task = command.getTask();
+		boolean isSuccess;
 
-    public Action getLastAction() {
-        return _lastAction;
-    }
+		switch (actionType) {
+		case ADD:
+			processAdd(command, task);
+			break;
+		case DELETE:
+			processDelete(command, task);
+			break;
+		case DISPLAY:
+			// now can only filter string
+			processDisplay(task);
+			break;
+		case EDIT:
+			processEdit(command, task);
+			break;
+		case UNDO:
+			// negate action and run excecuteAction again
+			processUndo(command);
+			break;
+		case DONE:
+			processDone(command, task);
+			break;
+		case LOGIN:
+			processLogin(command);
+			break;
+		case EXIT:
+			processExit();
+			break;
+		case INVALID:
+			// call print some invalid message
+			break;
+		default:
+			// throw error
+		}
+	}
 
-    public void setLastAction(Action lastAction) {
-        this._lastAction = lastAction;
-    }
+	private void processExit() {
+		System.exit(0);
+	}
+
+	private void processLogin(Action command) {
+		boolean isSuccess;
+		isSuccess = _storageHandler.initCloudStorage();
+		this.showStatusToUser(command, isSuccess);
+	}
+
+	private void processDone(Action command, Task task) {
+		task.setDone(true);
+		processEdit(command, task);
+	}
+
+	private void processUndo(Action command) {
+		executeAction(command.undo(this.getLastAction()));
+		FilterTasks.filter();
+		DisplayView.updateTaskView(FilterTasks.getFilteredList(), _gui);
+	}
+
+	private void processEdit(Action command, Task task) {
+		boolean isSuccess;
+		isSuccess = _storageHandler.modifyTask(task);
+		filterAndDisplay(command, isSuccess);
+	}
+
+	private void processDisplay(Task task) {
+		FilterTasks.filter(task);
+		DisplayView.updateTaskView(FilterTasks.getFilteredList(), _gui);
+	}
+
+	private void processDelete(Action command, Task task) {
+		boolean isSuccess;
+		isSuccess = _storageHandler.deleteTask(task);
+		filterAndDisplay(command, isSuccess);
+	}
+
+	private void processAdd(Action command, Task task) {
+		boolean isSuccess;
+		isSuccess = _storageHandler.addTask(task);
+		filterAndDisplay(command, isSuccess);
+	}
+
+	private void filterAndDisplay(Action command, boolean isSuccess) {
+		// set lastAction
+		this.setLastAction(command);
+		FilterTasks.filter();
+		showStatusToUser(command, isSuccess);
+		DisplayView.updateTaskView(FilterTasks.getFilteredList(), _gui);
+	}
+
+	private void showStatusToUser(Action command, boolean isSuccess) {
+		if (isSuccess == true) {
+			DisplayView.showStatusToUser(StatusType.MESSAGE, command, _gui);
+		} else {
+			DisplayView.showStatusToUser(StatusType.ERROR, command, _gui);
+		}
+	}
+
+	public Action getLastAction() {
+		return _lastAction;
+	}
+
+	public void setLastAction(Action lastAction) {
+		this._lastAction = lastAction;
+	}
 
 }
