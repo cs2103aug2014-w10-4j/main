ff0507e03373ddc4679caea3597b283821c08310
Ming Yi <bluedabedi@hotmail.com>
2014-11-08 22:41:50 +0800

    Update JUnitStorageAtd with Before and After

--- src/chirptask/testing/JUnitStorageAtd.java
+++ src/chirptask/testing/JUnitStorageAtd.java
@@ -1,3 +1,4 @@
+//@author A0111840W
 package chirptask.testing;
 
 import static org.junit.Assert.assertEquals;
@@ -8,6 +9,8 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Calendar;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import chirptask.storage.DeadlineTask;
@@ -16,28 +19,31 @@ import chirptask.storage.Task;
 import chirptask.storage.TimedTask;
 
 public class JUnitStorageAtd {
-
-    //@author A0111840W
-    /**
-     * This tests the local storage for the unforeseen circumstances 
-     * if the program allows values out of boundary.
-     * It also tests the correctness of the values when storing and
-     * reading the LocalStorage.
-     */
-	@Test
-	public void testLocalStorage() {
-        LocalStorage local = new LocalStorage();
+    private final int taskIdA = 1; 
+    
+    private final String taskA = "Task A";
+    private final String taskB = "Task B";
+    private final String taskC = "Task C";
+    
+    private int taskIdB = 0;
+    private int taskIdC = 0;
+    
+    private LocalStorage local;
+    private Task floatingTask;
+    private Task timedTask;
+    private Task deadlineTask;
+    
+    @Before
+    public void setupStorageAndSampleTasks() {
+        local = new LocalStorage();
         local.setUpJUnitTestXmlWriter(); //Start test storage from fresh
         
-	    int taskIdA = 1; //1
-        int taskIdB = LocalStorage.generateId(); //2
-        int taskIdC = LocalStorage.generateId(); //3
+        taskIdB = LocalStorage.generateId(); //2
+        taskIdC = LocalStorage.generateId(); //3
+        
         Long millisB1 = 1412763010000L; //Wed Oct 08 18:10:10 SGT 2014 in Epoch Millis
         Long millisB2 = 1412935810000L; //Fri Oct 10 18:10:10 SGT 2014 in Epoch Millis
         Long millisC = 1413108610000L; //Sun Oct 12 18:10:10 SGT 2014 in Epoch Millis
-        String taskA = "Task A";
-        String taskB = "Task B";
-        String taskC = "Task C";
 
         Calendar calB1 = Calendar.getInstance(); //Assume local time is SGT TimeZone
         calB1.setTimeInMillis(millisB1);
@@ -46,11 +52,19 @@ public class JUnitStorageAtd {
         Calendar calC = Calendar.getInstance(); //Assume local time is SGT TimeZone
         calC.setTimeInMillis(millisC);
         
-        Task floatingTask = new Task(taskIdA, taskA);
-        Task timedTask = new TimedTask(taskIdB, taskB, calB1, calB2);
-        Task deadlineTask = new DeadlineTask(taskIdC, taskC, calC);
-	    
-		
+        floatingTask = new Task(taskIdA, taskA);
+        timedTask = new TimedTask(taskIdB, taskB, calB1, calB2);
+        deadlineTask = new DeadlineTask(taskIdC, taskC, calC);
+    }
+
+    /**
+     * This tests the local storage for the unforeseen circumstances 
+     * if the program allows values out of boundary.
+     * It also tests the correctness of the values when storing and
+     * reading the LocalStorage.
+     */
+	@Test
+	public void testLocalStorage() {
 		//true if the task has been successfully stored
 		assertTrue(local.storeNewTask(floatingTask));
 		assertTrue(local.storeNewTask(timedTask));
@@ -80,7 +94,13 @@ public class JUnitStorageAtd {
 		deadlineTask = local.getTask(deadlineTask.getTaskId());
         assertNotEquals(taskC, deadlineTask.getDescription());
         assertEquals("", deadlineTask.getDescription());
-        
 	}
-
+	
+	@After
+	public void cleanUp() {
+	    local = null;
+	    floatingTask = null;
+	    timedTask = null;
+	    deadlineTask = null;
+	}
 }
