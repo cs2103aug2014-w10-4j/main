9b7ccd979348641b95a6dcaf6c5d18d158c0d273
Quan Yang <QuanYang@live.com>
2014-11-10 16:19:47 +0800

    Revert "correct an assertion error."
    This reverts commit cced30713cd36cc1ea39f330471ab794ba23a32c.

--- src/chirptask/gui/MainGui.java
+++ src/chirptask/gui/MainGui.java
@@ -242,7 +242,7 @@ public class MainGui extends Application implements NativeKeyListener {
     public boolean addNewTaskViewToDate(Calendar date, int taskId,
             String description, String time, boolean done) {
         assert date != null && description != null && !description.isEmpty()
-                && taskId < 0;
+                && taskId > -1;
 
         // Checks for duplicate taskId
         if (_taskIndexToId.contains(taskId)) {
